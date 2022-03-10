9c5b21b5f86961148d172d90cb0ef8d77c0c5af9
JackOSheaDev <C20371873@mytudublin.ie>
2022-03-10 18:47:24 +0000

    csvReader class has been improved with more error testing and is now fully commented.

--- src/machinelearningproject/CSVReader.java
+++ src/machinelearningproject/CSVReader.java
@@ -1,34 +1,50 @@
 package machinelearningproject;
+/*
+    Author:
+        Jack O'Shea
+    Date:
+        3/10/2022
+    Purpose:
+        The purpose of CSV reader is to read a csv file and extends the fileReader class.
+    Attributes:
+        All attributes are extended from fileReader.
+    Methods:
+        Constructor sets the file name.
+        setFileName checks if the file is in CSV format and creates the file object
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+ */
 
-public class CSVReader
+
+//Class which extends the fileReader class and implements a method to verify the file type before setting it.
+public class CSVReader extends fileReader
 {
-    File csvFile;
-    Scanner scanFile;
 
-
-
-    public CSVReader(String filename)
+    //Constructor which initialises the CSVReader object with the name and creates the file.
+    public CSVReader(String fileName)
     {
-        csvFile = new File(filename);
-        try
-        {
-            scanFile = new Scanner(csvFile);
-            while(scanFile.hasNextLine())
-            {
-
-                String currentLine = scanFile.nextLine();
-                System.out.println(currentLine);
-            }
-
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("File was not found. Please check the path");
-        }
-
+        //Calls method to set the name to whatever is passed.
+        setFileName(fileName);
     }
+
+
+    //Implemented version of setFilename which checks the file extension, it also recreates the fill object.
+    @Override
+    public void setFileName(String filename) {
+        if(!filename.endsWith(".csv"))
+        {
+            //If the file type is incorrect.
+            System.out.println("Invalid file type");
+            System.out.println("File has not been created and scanner is not operating");
+        }
+        else
+        {
+            //If the file type is correct the object is created.
+            System.out.println("Correct file type");
+            this.fileName = filename;
+            createFileObj();
+        }
+    }
+
+
+
 }
